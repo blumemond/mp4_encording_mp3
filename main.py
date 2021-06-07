687b2cab5a49ed207735253c4696bcba2cff76da
blumemond <75821564+blumemond@users.noreply.github.com>
2021-06-07 10:05:34 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import subprocess
+# import os
+
+# os.chdir(r'/Users/naoki_sakano/Desktop/')
+cmd = 'ffmpeg -i /Users/naoki_sakano/Desktop/zoom_0.mp4 /Users/naoki_sakano/Desktop/zoom_0.mp3'
+
+subprocess.call(cmd)
